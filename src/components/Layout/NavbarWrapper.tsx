e21f126c317188b8793f0be732e24ffc71806845
yyanxiao <55913268+yyanxiao@users.noreply.github.com>
2026-04-04 19:53:47 +0800

    Update NavbarWrapper.tsx

--- src/components/Layout/NavbarWrapper.tsx
+++ src/components/Layout/NavbarWrapper.tsx
@@ -2,16 +2,10 @@ import { Link } from "@heroui/link";
 import {
   Navbar,
   NavbarBrand,
-  NavbarContent,
-  NavbarItem,
 } from "@heroui/navbar";
-import { getUser } from '@/utils/auth';
-import AvatarWrapper from './AvatarWrapper';
 import ThemeSwitcher from './ThemeSwitcher';
 
 export default async function NavbarWrapper() {
-  const user = await getUser();
-
   return (
     <Navbar
       classNames={{
@@ -19,15 +13,11 @@ export default async function NavbarWrapper() {
       }}
     >
       <NavbarBrand className="gap-5">
-        {/* 1. 响应式瘦身：手机端使用 text-lg，电脑端恢复 text-xl 或更大 */}
         <Link href="/" className="text-lg md:text-xl font-bold tracking-wider">
           {process.env.BLOG_TITLE}
         </Link>
         <ThemeSwitcher />
       </NavbarBrand>
-      
-      {/* 2. 物理切除：已经彻底删除了 <NavbarContent> 和 <AvatarWrapper> 包含的登录按钮 */}
-      
     </Navbar>
   );
 }
