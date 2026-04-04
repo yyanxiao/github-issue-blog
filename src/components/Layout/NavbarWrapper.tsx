0c31b9a3406dbe3829a8d51ddddfd5ee1f64825a
yyanxiao <55913268+yyanxiao@users.noreply.github.com>
2026-04-04 19:30:56 +0800

    Update NavbarWrapper.tsx

--- src/components/Layout/NavbarWrapper.tsx
+++ src/components/Layout/NavbarWrapper.tsx
@@ -19,16 +19,15 @@ export default async function NavbarWrapper() {
       }}
     >
       <NavbarBrand className="gap-5">
-        <Link href="/" className="text-xl font-bold">
+        {/* 1. 响应式瘦身：手机端使用 text-lg，电脑端恢复 text-xl 或更大 */}
+        <Link href="/" className="text-lg md:text-xl font-bold tracking-wider">
           {process.env.BLOG_TITLE}
         </Link>
         <ThemeSwitcher />
       </NavbarBrand>
-      <NavbarContent justify="end">
-        <NavbarItem>
-          <AvatarWrapper user={user} />
-        </NavbarItem>
-      </NavbarContent>
+      
+      {/* 2. 物理切除：已经彻底删除了 <NavbarContent> 和 <AvatarWrapper> 包含的登录按钮 */}
+      
     </Navbar>
   );
 }
