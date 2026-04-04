554e8d30adb9cba133ade4aca8695728825dfceb
yyanxiao <55913268+yyanxiao@users.noreply.github.com>
2026-04-04 19:14:25 +0800

    Update index.tsx

--- src/components/PostEditor/index.tsx
+++ src/components/PostEditor/index.tsx
@@ -36,7 +36,7 @@ export default function PostEditor({
         classNames={{
           inputWrapper:
             'data-[hover=true]:bg-transparent group-data-[focus=true]:bg-transparent bg-transparent outline-none p-0',
-          input: '!text-primary font-bold text-3xl',
+          input: '!text-primary font-bold text-xl md:text-3xl',
         }}
       />
       <Textarea
