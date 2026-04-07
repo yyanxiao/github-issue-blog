d4b0b85a0090f851067055abce5feca76209f552
yyanxiao <55913268+yyanxiao@users.noreply.github.com>
2026-04-07 17:10:47 +0800

    Update Posts.tsx

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable react/require-default-props */
 'use client';
+
+/* eslint-disable react/require-default-props */
 
 import { InView } from 'react-intersection-observer';
 import { Link } from "@heroui/link";
@@ -12,7 +13,6 @@ export default function Posts({ data, tag }: { data: Issues; tag?: string }) {
   const { posts, noMorePosts, loadMore } = usePosts(data, tag);
 
   return (
-    // 核心修改：大幅削减列表项之间的垂直间距 (从 gap-8 md:gap-12 物理压缩至 gap-5 md:gap-7)
     <div className="flex flex-col gap-5 md:gap-7">
       {posts.map((post) => (
         <Link key={post.id} href={`/post/${post.number}`}>
