c076549bdae55cef7c6161d8fe2d91618639ef96
yyanxiao <55913268+yyanxiao@users.noreply.github.com>
2026-04-10 11:15:52 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { getPosts } from '@/utils/post';
 import { isAuthor } from '@/utils/auth';
 
 // 👇 核心核爆指令：强制打破永久数据缓存，设定 60 秒为数据的最长保质期
-export const revalidate = 60;
+export const revalidate = 3600;
 
 export default async function Home() {
   const data = await getPosts(1);
